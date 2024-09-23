96324269047bec331ee24d9a1882f57d279e043e
warnerfleming <fleming14@usf.edu>
2024-09-22 20:10:41 -0400

    Initialize employees with shift schedules

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -1,1 +1,9 @@
+//Task 1
 
+const employees = [
+    {name: `Dave`, shifts: [{day: `Monday`, hours: 6}, {day: `Friday`, hours: 4}]},
+    {name: `Steve`, shifts: [{day: `Tuesday`, hours: 8}, {day: `Saturday`, hours: 8}]},
+    {name: `Mary`, shifts: [{day: `Wedensday`, hours: 4}, {day: `Thursday`, hours: 4}]},
+    {name: `Greg`, shifts: [{day: `Monday`, hours: 8}]}
+
+]
